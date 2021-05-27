a5093c6e35292e96f5cd4fd78c2e5fdf568bce3b
Vadim <vadopolski@gmail.com>
2021-05-28 00:03:39 +0300

    Added groovy pipeline

--- pipeline.groovy
+++ pipeline.groovy
@@ -62,19 +62,24 @@ pipeline {
 
                 stage('batch') {
                     stages {
+
                         stage('build batching') {
-                            git 'https://github.com/vadopolski/batching.git'
+                            steps {
+                                git 'https://github.com/vadopolski/batching.git'
 
-                            sh "git checkout ${params.BRANCH}"
+                                sh "git checkout ${params.BRANCH}"
 
-                            sh "mvn clean package"
+                                sh "mvn clean package"
+                            }
                         }
 
                         stage('deploy batching') {
-                            sh """cd target
+                            steps {
+                                sh """cd target
                               curl -i -X PUT "http://namenode:9870/webhdfs/v1/batch_${params.BRANCH}/batching-1.0-SNAPSHOT.jar?op=CREATE&overwrite=true"
                               curl -i -X PUT -T test_jenkins_mvn-1.0-SNAPSHOT.jar "http://datanode:9864/webhdfs/v1/batch_${params.BRANCH}/batching-1.0-SNAPSHOT.jar?op=CREATE&namenoderpcaddress=namenode:9000&createflag=&createparent=true&overwrite=true"
-                           """
+                                   """
+                            }
                         }
 
                     }
