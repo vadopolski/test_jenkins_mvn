4c01b5dbd22240753556593f94580d54d62b9e78
Vadim <vadopolski@gmail.com>
2021-06-15 19:59:56 +0300

    Added url

---
+++ jenkins/test/testok.groovy
@@ -0,0 +1,28 @@
+pipeline {
+    agent any
+
+    stages {
+        stage("Clone sources") {
+            steps {
+                git url: 'https://github.com/tkgregory/sonarqube-jacoco-code-coverage.git'
+            }
+        }
+
+        stage("SonarQube analyses") {
+            steps {
+                withSonarQubeEnv('SonarQube') {
+                    sh "./gradlew sonarqube -Dsonar.login=c750434e7764c7e2ce18e66cb673f77ce212e644"
+                }
+            }
+        }
+
+        stage("Quality gate") {
+            steps {
+                waitForQualityGate abortPipeline: true
+            }
+        }
+    }
+}
+
+
+
